a2bd4d0d33b5cb0c904323cf5c77a73c12fddee8
Souranil Sen <souranil.sen@tcs.com>
2014-02-19 23:54:07 +0530

    Added to Date class
    * Added week_from_now and get_weeks
    * Added test cases

--- lib/week_calc.rb
+++ lib/week_calc.rb
@@ -1,9 +1,24 @@
 require 'date'
-
-class WeekCalc
+Date.class_eval do
   def self.get_weeks(start_date, end_date)
     #Calculations will return a date object
-    days_gap = Date.parse(end_date).mjd - Date.parse(start_date).mjd
-    weeks = days_gap / 7
+    start_date_parsed = Date.parse(start_date) rescue nil
+    end_date_parsed = Date.parse(end_date) rescue nil
+    if start_date_parsed.nil? && end_date_parsed.nil?
+      nil
+    else
+      days_gap = end_date_parsed.mjd - start_date_parsed.mjd
+      weeks = (days_gap/7).abs
+    end
+  end
+
+  def self.week_from_now(date)
+    date_parsed = Date.parse(date) rescue nil
+    if date_parsed.nil?
+      nil
+    else
+      days_gap = Date.today.mjd - date_parsed.mjd
+      weeks = (days_gap/7).abs
+    end
   end
 end
--- spec/week_calc_spec.rb
+++ spec/week_calc_spec.rb
@@ -1,1 +1,18 @@
 require 'week_calc'
+
+
+describe Date do
+
+  it "should return the number of weeks" do
+    Date.get_weeks('22/4/2012', '22/5/2012').should == 4
+  end
+
+  it "should return the number of weeks" do
+    Date.get_weeks('22/4/2012', '22/4/2012').should == 0
+  end
+
+  it "should return the number of weeks" do
+    Date.get_weeks('22/5/2012', '22/3/2012').should == 9
+  end
+
+end
