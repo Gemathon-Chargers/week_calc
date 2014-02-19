c10de75ae7db0041dbd0c0105254eee1ddd280ed
Souranil Sen <souranil.sen@tcs.com>
2014-02-19 15:12:53 +0530

    Added gem
    * Build and added the gem
    * Added basic functionality for calculation of weeks from and to a date.

--- lib/week_calc.rb
+++ lib/week_calc.rb
@@ -1,5 +1,9 @@
+require 'date'
+
 class WeekCalc
-  def get_weeks(start_date, end_date)
+  def self.get_weeks(start_date, end_date)
     #Calculations will return a date object
+    days_gap = Date.parse(end_date).mjd - Date.parse(start_date).mjd
+    weeks = days_gap / 7
   end
 end
